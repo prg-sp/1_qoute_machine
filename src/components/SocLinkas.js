4bbe28fc57ef53aa2efd3f9184ce044699250a17
Paulius NeSaulius <saulius.pukenas@gmail.com>
2021-10-08 17:09:23 +0300

    social linkas

---
+++ src/components/SocLinkas.js
@@ -0,0 +1,12 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+
+const SocLinkas  = ({cssClass, id, icon, spalva,hrefas}) => {
+	const eleme2 = <FontAwesomeIcon icon={icon} />
+	return (
+		<div>
+			<a style={{background: spalva}}id={id} href={hrefas} target="_blank"className={cssClass}>{eleme2}</a>
+		</div>
+		)
+}
+
+export default SocLinkas;
